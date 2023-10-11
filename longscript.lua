e0099df5abac455081994b37ce96b6ea3ea056d6
Slektus (Aka 9am) <96994334+slektus@users.noreply.github.com>
2023-10-11 11:20:06 -0400

    Create longscript.lua

---
+++ longscript.lua
@@ -0,0 +1,342 @@
+--Made by w6yu skids--
+
+local Converted = {
+	["_ZGui"] = Instance.new("ScreenGui");
+	["_Frame"] = Instance.new("Frame");
+	["_UICorner"] = Instance.new("UICorner");
+	["_TextLabel"] = Instance.new("TextLabel");
+	["_UICorner1"] = Instance.new("UICorner");
+	["_TextLabel1"] = Instance.new("TextLabel");
+	["_Ultra Spped"] = Instance.new("TextButton");
+	["_LocalScript"] = Instance.new("LocalScript");
+	["_UICorner2"] = Instance.new("UICorner");
+	["_Gun"] = Instance.new("TextButton");
+	["_UICorner3"] = Instance.new("UICorner");
+	["_LocalScript1"] = Instance.new("LocalScript");
+	["_TextLabel2"] = Instance.new("TextLabel");
+	["_Kill"] = Instance.new("TextButton");
+	["_UICorner4"] = Instance.new("UICorner");
+	["_LocalScript2"] = Instance.new("LocalScript");
+	["_Donate"] = Instance.new("TextButton");
+	["_UICorner5"] = Instance.new("UICorner");
+	["_LocalScript3"] = Instance.new("LocalScript");
+	["_Kick"] = Instance.new("TextButton");
+	["_UICorner6"] = Instance.new("UICorner");
+	["_Reviz admin"] = Instance.new("TextButton");
+	["_LocalScript4"] = Instance.new("LocalScript");
+	["_Open/Close GUI"] = Instance.new("TextButton");
+	["_UICorner7"] = Instance.new("UICorner");
+}
+
+-- Properties:
+
+Converted["_ZGui"].ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Converted["_ZGui"].Name = "ZGui"
+Converted["_ZGui"].Parent = game:GetService("CoreGui")
+
+Converted["_Frame"].BackgroundColor3 = Color3.fromRGB(75.00000312924385, 132.0000073313713, 118.00000816583633)
+Converted["_Frame"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Frame"].BorderSizePixel = 0
+Converted["_Frame"].Position = UDim2.new(0.0105871027, 0, 0.676923096, 0)
+Converted["_Frame"].Size = UDim2.new(0, 700, 0, 362)
+Converted["_Frame"].Parent = Converted["_ZGui"]
+
+Converted["_UICorner"].Parent = Converted["_Frame"]
+
+Converted["_TextLabel"].Font = Enum.Font.LuckiestGuy
+Converted["_TextLabel"].Text = "discord.gg/zillakami"
+Converted["_TextLabel"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel"].TextSize = 14
+Converted["_TextLabel"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextLabel"].BackgroundTransparency = 100
+Converted["_TextLabel"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel"].BorderSizePixel = 0
+Converted["_TextLabel"].Size = UDim2.new(0, 700, 0, 29)
+Converted["_TextLabel"].Parent = Converted["_Frame"]
+
+Converted["_UICorner1"].Parent = Converted["_TextLabel"]
+
+Converted["_TextLabel1"].Font = Enum.Font.LuckiestGuy
+Converted["_TextLabel1"].Text = "Handmade by w6yu"
+Converted["_TextLabel1"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel1"].TextSize = 14
+Converted["_TextLabel1"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextLabel1"].BackgroundTransparency = 100
+Converted["_TextLabel1"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel1"].BorderSizePixel = 0
+Converted["_TextLabel1"].Position = UDim2.new(0, 0, 0.939226508, 0)
+Converted["_TextLabel1"].Size = UDim2.new(0, 700, 0, 22)
+Converted["_TextLabel1"].Parent = Converted["_Frame"]
+
+Converted["_Ultra Spped"].Font = Enum.Font.LuckiestGuy
+Converted["_Ultra Spped"].Text = "ULTRA SPEED"
+Converted["_Ultra Spped"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Ultra Spped"].TextSize = 14
+Converted["_Ultra Spped"].BackgroundColor3 = Color3.fromRGB(97.00000941753387, 118.00000816583633, 255)
+Converted["_Ultra Spped"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Ultra Spped"].BorderSizePixel = 0
+Converted["_Ultra Spped"].Position = UDim2.new(0.0644315034, 0, 0.158011079, 0)
+Converted["_Ultra Spped"].Size = UDim2.new(0, 95, 0, 27)
+Converted["_Ultra Spped"].Name = "Ultra Spped"
+Converted["_Ultra Spped"].Parent = Converted["_Frame"]
+
+Converted["_UICorner2"].Parent = Converted["_Ultra Spped"]
+
+Converted["_Gun"].Font = Enum.Font.LuckiestGuy
+Converted["_Gun"].Text = "Gun"
+Converted["_Gun"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Gun"].TextSize = 14
+Converted["_Gun"].BackgroundColor3 = Color3.fromRGB(255, 114.00000840425491, 243.00001591444016)
+Converted["_Gun"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Gun"].BorderSizePixel = 0
+Converted["_Gun"].Position = UDim2.new(0.22857143, 0, 0.157458559, 0)
+Converted["_Gun"].Size = UDim2.new(0, 95, 0, 27)
+Converted["_Gun"].Name = "Gun"
+Converted["_Gun"].Parent = Converted["_Frame"]
+
+Converted["_UICorner3"].Parent = Converted["_Gun"]
+
+Converted["_TextLabel2"].Font = Enum.Font.LuckiestGuy
+Converted["_TextLabel2"].Text = "gun doesnt wokr, join dizzy for updates."
+Converted["_TextLabel2"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel2"].TextSize = 14
+Converted["_TextLabel2"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextLabel2"].BackgroundTransparency = 1
+Converted["_TextLabel2"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextLabel2"].BorderSizePixel = 0
+Converted["_TextLabel2"].Position = UDim2.new(0.265714288, 0, 0.0801104978, 0)
+Converted["_TextLabel2"].Size = UDim2.new(0, 327, 0, 15)
+Converted["_TextLabel2"].Parent = Converted["_Frame"]
+
+Converted["_Kill"].Font = Enum.Font.LuckiestGuy
+Converted["_Kill"].Text = "kys"
+Converted["_Kill"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Kill"].TextSize = 14
+Converted["_Kill"].BackgroundColor3 = Color3.fromRGB(251.000015437603, 255, 34.00000177323818)
+Converted["_Kill"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Kill"].BorderSizePixel = 0
+Converted["_Kill"].Position = UDim2.new(0.394285709, 0, 0.157458559, 0)
+Converted["_Kill"].Size = UDim2.new(0, 95, 0, 27)
+Converted["_Kill"].Name = "Kill"
+Converted["_Kill"].Parent = Converted["_Frame"]
+
+Converted["_UICorner4"].Parent = Converted["_Kill"]
+
+Converted["_Donate"].Font = Enum.Font.LuckiestGuy
+Converted["_Donate"].Text = "Donate"
+Converted["_Donate"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Donate"].TextSize = 14
+Converted["_Donate"].BackgroundColor3 = Color3.fromRGB(255, 0, 4.000000236555934)
+Converted["_Donate"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Donate"].BorderSizePixel = 0
+Converted["_Donate"].Position = UDim2.new(0.757142842, 0, 0.895027637, 0)
+Converted["_Donate"].Size = UDim2.new(0, 117, 0, 27)
+Converted["_Donate"].Name = "Donate"
+Converted["_Donate"].Parent = Converted["_Frame"]
+
+Converted["_UICorner5"].Parent = Converted["_Donate"]
+
+Converted["_Kick"].Font = Enum.Font.LuckiestGuy
+Converted["_Kick"].Text = "Kick (COMING SOON)"
+Converted["_Kick"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Kick"].TextSize = 14
+Converted["_Kick"].BackgroundColor3 = Color3.fromRGB(185.00000417232513, 73.00000324845314, 255)
+Converted["_Kick"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Kick"].BorderSizePixel = 0
+Converted["_Kick"].Position = UDim2.new(0.552857101, 0, 0.157458559, 0)
+Converted["_Kick"].Size = UDim2.new(0, 143, 0, 27)
+Converted["_Kick"].Name = "Kick"
+Converted["_Kick"].Parent = Converted["_Frame"]
+
+Converted["_UICorner6"].Parent = Converted["_Kick"]
+
+Converted["_Reviz admin"].Font = Enum.Font.LuckiestGuy
+Converted["_Reviz admin"].Text = "Reviz admin (coming soon)"
+Converted["_Reviz admin"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Reviz admin"].TextSize = 10
+Converted["_Reviz admin"].BackgroundColor3 = Color3.fromRGB(165.00000536441803, 0, 3.0000002961605787)
+Converted["_Reviz admin"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Reviz admin"].BorderSizePixel = 0
+Converted["_Reviz admin"].Position = UDim2.new(0.765714288, 0, 0.157458559, 0)
+Converted["_Reviz admin"].Size = UDim2.new(0, 154, 0, 27)
+Converted["_Reviz admin"].Name = "Reviz admin"
+Converted["_Reviz admin"].Parent = Converted["_Frame"]
+
+Converted["_Open/Close GUI"].Font = Enum.Font.LuckiestGuy
+Converted["_Open/Close GUI"].Text = "Mod Menu"
+Converted["_Open/Close GUI"].TextColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Open/Close GUI"].TextSize = 14
+Converted["_Open/Close GUI"].BackgroundColor3 = Color3.fromRGB(14.000001065433025, 243.00001591444016, 255)
+Converted["_Open/Close GUI"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Open/Close GUI"].BorderSizePixel = 0
+Converted["_Open/Close GUI"].Position = UDim2.new(0.0202117413, 0, 0.953846157, 0)
+Converted["_Open/Close GUI"].Size = UDim2.new(0, 100, 0, 27)
+Converted["_Open/Close GUI"].Name = "Open/Close GUI"
+Converted["_Open/Close GUI"].Parent = Converted["_ZGui"]
+
+Converted["_UICorner7"].Parent = Converted["_Open/Close GUI"]
+
+-- Fake Module Scripts:
+
+local fake_module_scripts = {}
+
+
+-- Fake Local Scripts:
+
+local function SDAEOMA_fake_script() -- Fake Script: StarterGui.ZGui.Frame.Ultra Spped.LocalScript
+    local script = Instance.new("LocalScript")
+    script.Name = "LocalScript"
+    script.Parent = Converted["_Ultra Spped"]
+    local req = require
+    local require = function(obj)
+        local fake = fake_module_scripts[obj]
+        if fake then
+            return fake()
+        end
+        return req(obj)
+    end
+
+	print("Ultraspeed executed, .gg/zillakami")
+	
+	local runService = game:GetService("RunService")
+	local userInputService = game:GetService("UserInputService")
+	
+	local character = game.Players.LocalPlayer.Character
+	local hrp = character:WaitForChild("HumanoidRootPart")
+	local camera = game.Workspace.CurrentCamera
+	
+	local bodyPos = Instance.new("BodyPosition", hrp)
+	bodyPos.MaxForce = Vector3.new()
+	bodyPos.D = 0
+	
+	local bodyGyro = Instance.new("BodyGyro", hrp)
+	bodyGyro.MaxTorque = Vector3.new()
+	bodyGyro.D = 10
+	
+	local speed = 1
+	local isFlying = false
+	
+	local toggleKey = Enum.KeyCode.F
+	
+	local button = script.Parent
+	
+	function fly()
+		isFlying = true
+		bodyPos.MaxForce = Vector3.new(100000, 100000, 100000)
+		bodyGyro.MaxTorque = Vector3.new(100000, 100000, 100000)
+		runService:BindToRenderStep("fly", 1, function()
+			bodyPos.Position = hrp.Position + ((hrp.Position - camera.CFrame.p).unit * speed)
+			bodyGyro.CFrame = CFrame.new(camera.CFrame.p, hrp.Position)
+		end)
+	end
+	
+	function stopFlying()
+		isFlying = false
+		runService:UnbindFromRenderStep("fly")
+		bodyPos.MaxForce = Vector3.new()
+		bodyGyro.MaxTorque = Vector3.new()
+	end
+	
+	button.Activated:Connect(function()
+		if not isFlying then
+			fly()
+		else
+			stopFlying()
+		end
+	end)
+	
+	userInputService.InputBegan:Connect(function(input, isProcessed)
+		if not isProcessed and input.KeyCode == toggleKey then
+			stopFlying()
+		end
+	end)
+	
+end
+local function ZFSFUY_fake_script() -- Fake Script: StarterGui.ZGui.Frame.Gun.LocalScript
+    local script = Instance.new("LocalScript")
+    script.Name = "LocalScript"
+    script.Parent = Converted["_Gun"]
+    local req = require
+    local require = function(obj)
+        local fake = fake_module_scripts[obj]
+        if fake then
+            return fake()
+        end
+        return req(obj)
+    end
+
+	print(".gg/zillakami")
+	
+	local tool = game.ReplicatedStorage.FullAuto --tool name goes here--
+	local player = game.Players.LocalPlayer
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		local clone = tool:Clone()
+		clone.Parent = player.Backpack
+	end)
+end
+local function BOBNRAC_fake_script() -- Fake Script: StarterGui.ZGui.Frame.Kill.LocalScript
+    local script = Instance.new("LocalScript")
+    script.Name = "LocalScript"
+    script.Parent = Converted["_Kill"]
+    local req = require
+    local require = function(obj)
+        local fake = fake_module_scripts[obj]
+        if fake then
+            return fake()
+        end
+        return req(obj)
+    end
+
+	print("kys script executing")
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.Character.Humanoid.Health = 0
+	end)
+end
+local function EKABD_fake_script() -- Fake Script: StarterGui.ZGui.Frame.Donate.LocalScript
+    local script = Instance.new("LocalScript")
+    script.Name = "LocalScript"
+    script.Parent = Converted["_Donate"]
+    local req = require
+    local require = function(obj)
+        local fake = fake_module_scripts[obj]
+        if fake then
+            return fake()
+        end
+        return req(obj)
+    end
+
+	print("Zgui Loaded!")
+	
+	
+end
+local function BGWF_fake_script() -- Fake Script: StarterGui.ZGui.LocalScript
+    local script = Instance.new("LocalScript")
+    script.Name = "LocalScript"
+    script.Parent = Converted["_ZGui"]
+    local req = require
+    local require = function(obj)
+        local fake = fake_module_scripts[obj]
+        if fake then
+            return fake()
+        end
+        return req(obj)
+    end
+
+	print(".gg/zillakami")
+	
+	local SGui = script.Parent
+	local Frame = SGui:WaitForChild("Frame")
+	local Button = SGui:WaitForChild("Open/Close GUI")
+	
+	Button.MouseButton1Up:Connect(function()
+		Frame.Visible = not Frame.Visible
+	end)
+end
+
+coroutine.wrap(SDAEOMA_fake_script)()
+coroutine.wrap(ZFSFUY_fake_script)()
+coroutine.wrap(BOBNRAC_fake_script)()
+coroutine.wrap(EKABD_fake_script)()
+coroutine.wrap(BGWF_fake_script)()
